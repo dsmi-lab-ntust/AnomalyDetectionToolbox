27f697ad8d0f0b8454bf564d43b3251094b3b96b
dsmi-lab-ntust <M10115088@mail.ntust.edu.tw>
2014-03-11 18:12:04 +0800

    Fix typos

--- distributionBased/LOF.m
+++ distributionBased/LOF.m
@@ -3,7 +3,7 @@ function [suspicious_index lof] = LOF(A, k)
 %# Local Outlier Factor                                              #
 %# Authors: Markus M. Breunig, Hans-Peter Kriegel,                   # 
 %#          Raymond T. Ng, J?rg Sander                               #
-%# Oringinal paper :                                                 #
+%# Original paper :                                                  #
 %# LOF: Identifying Density-Based Local Outliers                     #
 %# e-mail : { breunig | kriegel | sander }                           #
 %#          @dbs.informatik.uni-muenchen.de                          #
@@ -13,13 +13,13 @@ function [suspicious_index lof] = LOF(A, k)
 %#                                                                   #
 %#                                                                   #
 %# Inputs                                                            #
-%#   A: the data matrix, each row reprent an instance                #
+%#   A: the data matrix, each row represents an instance             #
 %#   k: the number of nearest neighbors                              #
 %#                                                                   #
 %# Outputs                                                           #
 %#   lof: the local outlier factor for each instance                 #
 %#   suspicious_index: the ranking of instances according to their   #
-%#                     suspiciuous score                             #
+%#                     suspicious score                              #
 %#                     For example, suspicious_index(i)=j means the  #
 %#                     ith instance is in jth position in the ranking#
 %#####################################################################
@@ -33,13 +33,13 @@ k_index = k_index(:,2:end);
 k_dist1 = k_dist(:,end);
 %Get row length of matrix A
 n = length(A(:,1));
-%Initializa lrd_value vector
+%Initialize lrd_value vector
 lrd_value = zeros(n,1);
 %Calculate lrd for each elements
 for i = 1:n
     lrd_value(i) = lrd(A, i, k_dist1,k_index, k);
 end
-%Initializa lof vector
+%Initialize lof vector
 lof = zeros(n,1);
 %Calculate LOF
 for i = 1:n
