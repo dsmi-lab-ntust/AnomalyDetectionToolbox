03bbc5704aa2501c3f4914af723f46012e462ec0
fnaith <weichih.fr.lai@gmail.com>
2014-06-16 10:28:49 +0800

    remove RBRP as a junk

--- RBRP/RBRP.m
+++
@@ -1,8 +0,0 @@
-function O = RBRP(k, n, B)
-% Fast Mining of Distance-Based Outliers in High-Dimensional Datasets
-% Inputs:
-%   : 
-% Outputs:
-%  O: t
-    D_k_min = 0; % Reset the threshold pruning
-    O; % Make empty the outlier result set
