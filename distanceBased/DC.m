2d0f09dcf03c3d23fceffec7f400d2a8a36fe956
fnaith <weichih.fr.lai@gmail.com>
2014-07-03 13:30:41 +0800

    fix CD.m

--- distanceBased/DC.m
+++ distanceBased/DC.m
@@ -1,5 +1,10 @@
-function [C, C_dist] = DC(S_prefix, S_number, S_profix, r)
+function [C, C_index, C_dist] = DC(S_prefix, S_number, S_profix, r, k)
 % Disk Aware Discord Discovery: Finding Unusual Time Series in Terabyte Sized Datasets
 % http://www.cs.ucr.edu/~eamonn/DiskawareDiscords.pdf
-    [C C_index] = DC_Selection(S_prefix, S_number, S_profix, r);
+    [C, C_index] = DC_Selection(S_prefix, S_number, S_profix, r);
     [C, C_dist] = DC_Refinement(S_prefix, S_number, S_profix, C, C_index, r);
+    [~, find] = sort(C_dist, 'descend');
+    find = find(1:min(k, size(find, 1)), :);
+    C = C(find, :);
+    C_index = C(find, :);
+    C_dist = C(find, :);
