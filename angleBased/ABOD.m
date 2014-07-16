93768361d15c71c7c0e1bdf7e5c3e9b21f98eabc
fnaith <weichih.fr.lai@gmail.com>
2014-07-16 10:29:58 +0800

    fix ABOD.m

--- angleBased/ABOD.m
+++ angleBased/ABOD.m
@@ -24,33 +24,39 @@ function [suspicious_index abof] = ABOD(A)
 %# Note                                                              #
 %#   each value in output abof was normalized to [0, 1]              #
 %#####################################################################
-n=length(A(:,1));
-var_array=zeros(n,1);
-for i=1:n
-    var_front=0;
-    var_back=0;
-    denominator=0;    
-    for j=1:n
-        if j==i
-            continue
-        end
-        for k=j+1:n
-           if k==i 
+    [A, ia, ic] = unique(A,'rows');
+    instance_number = size(ia, 1);
+    origin_instance_number = size(ic, 1);
+    var_array = zeros(instance_number, 1);
+    for i=1:instance_number
+        var_front = 0;
+        var_back = 0;
+        denominator = 0;    
+        for j=1:instance_number
+            if j==i
                 continue
-           end
-           vector1=A(j,:)-A(i,:);
-           vector2=A(k,:)-A(i,:);
-           norm_vector1Xnorm_vector2 = norm(vector1)*norm(vector2);
-           vector1Xvector2T = vector1*vector2';
-           var_front=var_front + (1/norm_vector1Xnorm_vector2) * (vector1Xvector2T/(norm_vector1Xnorm_vector2^2))^2;
-           var_back=var_back + (vector1Xvector2T/norm_vector1Xnorm_vector2^3);
-           denominator=denominator + (1/norm_vector1Xnorm_vector2);
+            end
+            for k=j+1:instance_number
+               if k==i 
+                    continue
+               end
+               vector1 = A(j,:) - A(i,:);
+               vector2 = A(k,:) - A(i,:);
+               norm_vector1Xnorm_vector2 = norm(vector1) * norm(vector2);
+               vector1Xvector2T = vector1 * vector2';
+               var_front = var_front + (1 / norm_vector1Xnorm_vector2) * (vector1Xvector2T / (norm_vector1Xnorm_vector2 ^ 2)) ^ 2;
+               var_back = var_back + (vector1Xvector2T / norm_vector1Xnorm_vector2 ^ 3);
+               denominator = denominator + (1 / norm_vector1Xnorm_vector2);
+            end
         end
+        var_array(i) = var_front / denominator - (var_back / denominator) ^ 2;
     end
-    var_array(i)=var_front/denominator-(var_back/denominator)^2;
-end
 
-% normalize the abof
-min_var_array = min(var_array);
-abof = (var_array-min_var_array)/(max(var_array)-min_var_array);
-[~,suspicious_index]=sort(abof);
+    min_var_array = min(var_array);
+    abof = (var_array - min_var_array) / (max(var_array) - min_var_array);
+    origin_abof = zeros(origin_instance_number, 1);
+    for i=1:origin_instance_number
+        origin_abof(i, 1) = abof(ic(i, 1), 1);
+    end
+    abof = origin_abof;
+    [~, suspicious_index] = sort(abof);
