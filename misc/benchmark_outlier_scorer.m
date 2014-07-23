ed1733bfad29a1a20b70ee44cd6c03473b044c31
absolutelyNoWarranty <M10115088@mail.ntust.edu.tw>
2014-07-24 05:48:28 +0800

    Modify benchmark_outlier_scorer Extra return argument, the anomaly scores. Extra input argument, posclass for AUC calculation.

--- misc/benchmark_outlier_scorer.m
+++ misc/benchmark_outlier_scorer.m
@@ -1,4 +1,4 @@
-function roc_auc = benchmark_outlier_scorer(A, labels, outlier_scorer)
+function [roc_auc scores] = benchmark_outlier_scorer(A, labels, outlier_scorer, posclass)
 %benchmark_outlier_scorer A convenient wrapper for benchmarking 
 %   Input:
 %   A - a mxn dataset, columns are features, rows are data
@@ -9,11 +9,30 @@ function roc_auc = benchmark_outlier_scorer(A, labels, outlier_scorer)
 %                    Should be of the form
 %       
 %                    function SCORES = FUN(A)
+%   posclass - the positive class label (scalar), either
+%    numeric (for numeric LABELS), logical (for logical LABELS) or char
+%    (for function perfcurve)
+%    If not specified and the datatype of labels is numeric or boolean, 
+%    will assume is either 1 or true, respectively. Otherwise an error will
+%    be returned.
 %   Output:
 %   metrics: a struct containing various metrics based on the result of
 %            running outlier_scorer on A with respect to labels
 
+assert(length(unique(labels)) == 2);
+if nargin < 4
+    if islogical(labels)
+        posclass = true;
+    elseif isnumeric(labels)
+        posclass = 1;
+    else
+        error('What is the positive class label?');
+    end
+end
+
+
+
 scores = outlier_scorer(A);
-[~, ~, ~, roc_auc] = perfcurve(labels, scores, 1);
+[~, ~, ~, roc_auc] = perfcurve(labels, scores, posclass);
 end
 
