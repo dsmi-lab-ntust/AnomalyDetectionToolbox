1a0305c1843be5435120dde2d5774680d5c2c5cc
jingyao <jyl.ntust@gmail.com>
2014-08-27 11:46:04 +0800

    DTW example updated

--- distanceBased/DTW_AF/example.m
+++ distanceBased/DTW_AF/example.m
@@ -6,33 +6,40 @@ cd util;
 [ unseenSeqCell ] = cellReader( [rootPath '\benchmarks\Time Series Data\Synthetic Wave\unseenSeqs.data.csv'] );
 
 figure;
-title('Normal Synthetic Waves')
+title('Part of Concatenated Normal Synthetic Waves')
 hold on;
-for i = 1:length(normalSeqCell)
-    plot(normalSeqCell{i});
+s=[];
+for i = 1:10
+    s=[s normalSeqCell{i}];
 end
-
+plot(s);
+axisNormal =axis;
+axis([axisNormal(1) axisNormal(2) axisNormal(3) axisNormal(4)*2.0])
 cd(currentPath);
 tic
 [score  ] = DTW_AF( unseenSeqCell(1),normalSeqCell,5 ) % it is heavy to compute median of k-distance
 figure;
 hold on;
-title('Unseen Synthetic Wave 1')
-for i = 1:length(normalSeqCell)
-    plot(normalSeqCell{i});
-end
-plot(unseenSeqCell{1},'r');
-disp(['Anomaly score of unseen synthetic wave 1 is' num2str(score)])
+title('Unseen Synthetic Wave 1 (normal)')
+% for i = 1:length(normalSeqCell)
+%     plot(normalSeqCell{i});
+% end
+plot(unseenSeqCell{1});
+axisCurrent = axis;
+axis([axisCurrent(1) axisCurrent(2) axisNormal(3) axisNormal(4)*2.0])
+disp(['Anomaly score of unseen synthetic wave 1 (normal) is ' num2str(score)])
 toc
 tic
 [score  ] = DTW_AF( unseenSeqCell(38),normalSeqCell,5 ) % it is heavy to compute median of k-distance
 figure;
 hold on;
-title('Unseen Synthetic Wave 38')
-for i = 1:length(normalSeqCell)
-    plot(normalSeqCell{i});
-end
+title('Unseen Synthetic Wave 38 (abnormal)')
+% for i = 1:length(normalSeqCell)
+%     plot(normalSeqCell{i});
+% end
 plot(unseenSeqCell{38},'r');
-disp(['Anomaly score of unseen synthetic wave 38 is' num2str(score)])
+axisCurrent = axis;
+axis([axisCurrent(1) axisCurrent(2) axisNormal(3) axisNormal(4)*2.0])
+disp(['Anomaly score of unseen synthetic wave 38 (normal) is ' num2str(score)])
 toc
 clearvars currentPath rootPath i score 
